fb22eee4eea891f005c9aa9acdd717e3d241c27b
Hemapriya <hemapriyak509@gmail.com>
2026-07-16 22:53:58 +0530

    Time: 4 ms (84.67%), Space: 46.4 MB (58.78%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1); int result = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') stack.push(i);
+            else {
+                stack.pop();
+                if(stack.isEmpty()) stack.push(i);
+                else result = Math.max(result, i - stack.peek());
+            }
+        }
+        return result;
+    }
+}

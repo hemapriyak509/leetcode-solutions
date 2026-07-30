ffcc9383cf9af4fae9ce9eabd93d3d051325d5d6
Hemapriya <hemapriyak509@gmail.com>
2026-07-30 20:28:30 +0530

    Time: 1 ms (92.71%), Space: 42.8 MB (31.95%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,11 +1,34 @@
-import java.math.*;
 class Solution {
     public int divide(int dividend, int divisor) {
+        if (dividend == divisor)
+            return 1;
+
         
-        int quo = dividend / divisor;
-        if(dividend==Integer.MIN_VALUE && divisor==-1){
-            return Integer.MAX_VALUE;
+        boolean isPositive = (dividend < 0) == (divisor < 0);
+
+        
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+        long ans = 0;
+
+        
+        while (a >= b) {
+            int q = 0;
+            
+            
+            while (a > (b << (q + 1)))
+                q++;
+            
+            
+            ans += (1L << q);
+            a -= (b << q);
         }
-        return quo;
+
+        
+        if (ans == (1L << 31) && isPositive)
+            return Integer.MAX_VALUE;
+
+        
+        return isPositive ? (int) ans : (int) -ans;
     }
 }

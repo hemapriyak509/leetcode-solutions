2c29dcee9d3079f3a4d85589b2e03cafa23c0440
Hemapriya <hemapriyak509@gmail.com>
2026-08-05 20:19:52 +0530

    Time: 0 ms (100%), Space: 46.7 MB (73.41%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        if(head.val == Integer.MAX_VALUE) return true;
+        head.val = Integer.MAX_VALUE;
+        return hasCycle(head.next);
+    }
+}

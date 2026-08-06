6f7d70ade74795480b54ce6bd397981f7ec05d6a
Hemapriya <hemapriyak509@gmail.com>
2026-08-06 20:34:41 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.06%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int tribonacci(int n) {
+        int sum=0;
+        if(n==1 || n==2)return 1;
+        int num1=0,num2=0,num3=1;
+        while(n-- > 1)
+            {
+                sum=num1+num2+num3;
+                num1=num2;
+                num2=num3;
+                num3=sum;
+            }
+        return sum;
+    }
+}
